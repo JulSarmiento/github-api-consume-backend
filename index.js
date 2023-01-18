bd1c14fe4571f5ebb70441534064b982a781a6ff
JulSarmiento <jasa1999@hotmail.com>
2023-01-18 16:25:47 -0500

    Listando repositorios publico segun usuario y token de github

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Octokit, App } = require("octokit");
+require("dotenv").config();
+
+/**
+ * @param {string} username
+ * @returns {Promise<string[]>}
+ */
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+/**
+ * @returns {Promise<string[]>}
+ * @param {string} username
+ */
+const repos = async () => {
+  const data = await octokit.request(`GET /users/${process.env.GITHUB_USERNAME}/repos`, {username: `${process.env.GITHUB_USERNAME}`});
+  const repos = Object.entries(data.data).map((repo) => {
+    return repo[1].name;
+  });
+
+  console.table(repos)
+}
+
+repos();
